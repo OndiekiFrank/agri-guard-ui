d3e16ae45bc8fe68e854a6e6808557d2bd3538c9
OndiekiFrank <frankline.ondieki1@student.moringaschool.com>
2025-09-13 04:43:00 +0300

    Render-ready TS + Vite config with Node16 ESM

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'  // remove .tsx extension
+import App from './App' // remove .tsx extension
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
